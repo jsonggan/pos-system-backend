33eab8cdc4fa853d68b0a601224bdeed0822901e
chinsong <1006283@mymail.sutd.edu.sg>
2023-10-11 17:16:46 +0800

    create tenant account

--- src/controllers/auth/tenantController.js
+++ src/controllers/auth/tenantController.js
@@ -2,21 +2,53 @@ const jwt = require('jsonwebtoken');
 const QRCode = require('qrcode');
 require('dotenv').config();
 const { v4: uuidv4 } = require('uuid');
-let tenantId = uuidv4(); // This will generate a unique tenant ID
+const knex = require('../../services/database/knex.js');
 
-// Generate JWT
-const token = jwt.sign({ foo: 'bar' }, process.env.SECRET_KEY, { expiresIn: '1h' });
+// // Generate JWT
+// const token = jwt.sign({ foo: 'bar' }, process.env.SECRET_KEY, { expiresIn: '1h' });
 
-// Generate QR code
-QRCode.toDataURL(`https://jsonggan.github.io/pos-system/?token=${token}`, function (err, url) {
-});
+// // Generate QR code
+// QRCode.toDataURL(`https://jsonggan.github.io/pos-system/?token=${token}`, function (err, url) {
+// });
 
 exports.createTenant = async (req, res) => {
-  // console.log(req.headers.authorization);
-  // console.log(req.body.email);
-  const token = req.headers.authorization;
-  console.log(JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString()));
-  res.send("correct path")
+  if (!req.body.email) {
+    console.log(req.body)
+    return res.status(400).send("Email is required");
+  }
+
+  if (!req.body.last_name) {
+    return res.status(400).send("Last Name is required");
+  }
+
+  const whiteListedEmails = await knex('white_listed_email').pluck('email');
+
+  if (!whiteListedEmails.includes(req.body.email)) {
+    return res.status(400).send("Please access makan official website for more about email authentication!");
+  }
+
+  const existingEmails = await knex('tenant').pluck('email_address');
+
+  if (existingEmails.includes(req.body.email)) {
+    return res.status(400).send("You are only allowed to have one account");
+  }
+
+  try {
+    let tenantId = uuidv4();
+    await knex('tenant').insert({
+      email_address: req.body.email,
+      last_name: req.body.last_name,
+      designation: req.body.designation,
+      first_name: req.body.first_name,
+      tenant_id: tenantId
+    })
+
+    const tenant = await knex('tenant').where('tenant_id', tenantId).first();
+    res.status(200).send({ message: "Tenant created successfully", tenant });
+  } catch (err) {
+    console.log(err)
+    return res.status(500).send("Error creating tenant, Please contact makan for existence");
+  }
 };
 
 exports.login = async (req, res) => {
